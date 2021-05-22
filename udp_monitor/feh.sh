17732f7b5c20fd6469c7ff9d9601bef995680df6
Wataru KUNINO <git@bokunimo.net>
2021-05-23 01:31:28 +0900

    コメントの追加

--- udp_monitor/feh.sh
+++ udp_monitor/feh.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+# Picturesフォルダ内の写真をスライドショー表示します
+
+# インストール方法
+#   sudo apt-get install feh
+#   git clone https://bokunimo.net/git/udp.git
+
+# 使用方法:
+#   fehを自動起動するには以下のコマンドを/etc/rc.localに入れておく
+#       sudo -u pi /home/pi/feh.sh delay &
 #
-# fehを自動起動するには以下のコマンドを/etc/rc.localに入れておく
-#
-# sudo -u pi /home/pi/feh.sh delay &
-#
+
+echo "Usage:" $0 "[start|stop|delay]"
+
 PID=`pidof feh`
 if [ "${PID}" != "" ] || [ "${1}" = "stop" ]; then
         kill $PID
