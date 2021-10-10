422ba67fec81967f45b0c39727745569f1a28b3d
Wataru KUNINO <git@bokunimo.net>
2021-10-10 20:59:37 +0900

    永久ループ版

--- learning/ex2_rx_loop.py
+++ learning/ex2_rx_loop.py
@@ -4,18 +4,23 @@
 # UDPを受信する
 # Copyright (c) 2021 Wataru KUNINO
 
-# UDPで受信した文字列を表示します。
+# UDPの待ち受けと、受信した文字列の表示を繰り返します。
+# ex2_rx.pyとの違いは、一度、実行すると繰り返し動作し続ける点と、
+# Listening UDP portや、送信元を表示しない点です。
 # ./ex2_rx_loop.py
+
+# 受信データをファイルに保存し続けることも出来ます。
+# ./ex2_rx_loop.py > log.csv &
 
 import socket                                               # ソケットの組み込み
 
 port = 1024                                                 # ポート番号を代入
 sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)       # ソケットを作成
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)   # オプション設定
-print('Listening UDP port', port, '...')                    # ポート番号表示
+# print('Listening UDP port', port, '...')                  # ポート番号表示
 
 sock.bind(('', port))                                       # ポート番号を設定
 while True:                                                 # 繰り返し構文
     udp = sock.recvfrom(128)                                # UDPパケットを取得
-    print(udp[1], udp[0].decode())                          # 受信データを表示
+    print(udp[0].decode())                                  # 受信データを表示
 sock.close()                                                # 切断(実行されない)
